60c59e3804b94e4e1b2b395635dd0d4b6d64d3b7
Hiléo Andersson <hileomsi@gmail.com>
2020-12-16 10:40:37 -0300

    chore(service) pass initializing properties

--- lib/services/sentry.service.ts
+++ lib/services/sentry.service.ts
@@ -20,13 +20,9 @@ export class SentryService extends Logger {
           // console.log('options not found. Did you use SentryModule.forRoot?');
           return;
         }
-
+        const { debug, integrations = [], ...sentryOptions } = options;
         Sentry.init({
-          dsn: options.dsn,
-          debug: options.debug === true ? false : options.debug,
-          environment: options.environment,
-          release: options.release,
-          logLevel: options.logLevel,
+          ...sentryOptions,
           integrations: [
             new Sentry.Integrations.OnUncaughtException({
               onFatalError: (async (err) => {
@@ -35,7 +31,8 @@ export class SentryService extends Logger {
                 if (err.name === 'SentryError') { console.log(err); } else { (Sentry.getCurrentHub().getClient<Client<Options>>() as Client<Options>).captureException(err); process.exit(1); }
               }),
             }),
-            new Sentry.Integrations.OnUnhandledRejection({mode: 'warn'})
+            new Sentry.Integrations.OnUnhandledRejection({mode: 'warn'}),
+            ...integrations
           ]
         });
       }
