045cea0fb97044d6ce0318b7bd8f695be6a83dea
Hiléo Andersson <hileomsi@gmail.com>
2020-12-16 18:01:02 -0300

    chore(build) add build folder

---
+++ dist/sentry.constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SENTRY_MODULE_OPTIONS = 'SentryModuleOptions';
+exports.SENTRY_TOKEN = 'SentryToken';
+//# sourceMappingURL=sentry.constants.js.map
---
+++ dist/sentry.decorator.d.ts
@@ -0,0 +1,1 @@
+export declare function InjectSentry(): (target: object, key: string | symbol, index?: number | undefined) => void;
